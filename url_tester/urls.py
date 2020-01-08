892bf50469e2055ae6aed729f5620a8ee19a56b3
Pavel Vasilev <bamboo2panda@gmail.com>
2020-01-08 16:19:54 +0300

    Add API app.

--- url_tester/urls.py
+++ url_tester/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'api/v0', include('api_v0.urls')),
 ]
